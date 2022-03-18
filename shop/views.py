340e48aa2d53a5092d3ee8731991844f5b20bb27
Razatvir <you@example.com>
2022-03-19 00:31:36 +0530

    version1

---
+++ shop/views.py
@@ -0,0 +1,60 @@
+import re
+from django.shortcuts import render
+from .models import Product, Contact
+from math import ceil
+
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    if request.method=="POST":
+        name = request.POST.get('name' ,'')
+        email  = request.POST.get('email' ,'')
+        phone = request.POST.get('phone' ,'0')
+        desc = request.POST.get('desc' ,'')
+        state = request.POST.get('state' ,'')
+        city = request.POST.get('city' ,'')
+        zip = request.POST.get('zip' ,'0')
+        contact = Contact(name= name, email =email, phone=phone,desc =desc,city =city,zip =zip, state=state)
+        contact.save()
+
+       
+    return render(request, 'shop/contact.html')
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productView(request,myid):
+    product = Product.objects.filter(id=myid)
+    return render(request, 'shop/prodView.html', {'product': product[0]})
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
